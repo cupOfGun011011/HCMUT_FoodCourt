729e7c66ea5a849d692963bd25415191c80e161f
Drakonair <redface112@gmail.com>
2020-07-30 17:34:10 +0700

    menu index.js

--- front-end/src/menu/index.js
+++ front-end/src/menu/index.js
@@ -1,4 +1,4 @@
-const serverAPI = "http://localhost:3001/";
+const serverAPI = "http://localhost:3001";
 const axios = require("axios");
 const instance = axios.create(({
     headers:{Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWVjZDIxMDc0YzcyZjA5ZjRhMjMzYzgiLCJpYXQiOjE1OTI1Nzg1NzZ9.nftSOwz7M_nk_3grSCeIBfHX0HZ9CgxYklvw8-YgnAs"}
@@ -7,56 +7,40 @@ instance.interceptors.request.use(request => {
     console.log('Starting Request', request)
     return request
   })
-let input = {
-    productID: 1,
-    ammount: 1,
-};
+let output; 
 const test = async ()=>{
      try {
-          output = await instance.get('${serverAPI}/products');
+          output = await instance.get(`${serverAPI}/products`);
           console.log(output);
           let markup = 
           output.data.map(el=>{
-
           let datajpg = "data:image/jpg;base64," + el.image;
-          return  "<td class="featured__item__pic set-bg">
-          //Hien thi image
-          <img
-             class = "item"
-             src = ${datajpg}
-             alt = ""
-          />
-          //
-          <h5>${el.name}</h5>
-                    </td>
-          <td class="shoping__cart__price">
-                               <div class="col-lg-3 col-md-4 col-sm-6 mix Food">
-            <div class="featured__item">
-              <div
-                class="featured__item__pic set-bg"
-                data-setbg="img/featured/feature-1.jpg"
-              >
-                <ul class="featured__item__pic__hover">
-                  <li>
-                    <a href="#"><i class="fa fa-shopping-cart"></i></a>
-                  </li>
-                </ul>
-              </div> -->
-              <!-- <div class="featured__item__text">
-                <h6><a href="#">Bánh mì</a></h6>
-                <h5>15000 VNĐ</h5>
-              </div>
-            </div>
-          </div>"
+          return `<div
+            class="featured__item__pic set-bg"
+            data-setbg=<img
+            src=${datajpg}
+            >
+        >
+          <ul class="featured__item__pic__hover">
+            <li>
+              <a href="#"><i class="fa fa-shopping-cart"></i></a>
+            </li>
+          </ul>
+        </div>
+        <div class="featured__item__text">
+          <h6><a href="#">${el.name}</a></h6>
+          <h5>${el.price} VNĐ</h5>
+        </div>`
           })
-       document.querySelector(".shopping__body").insertAdjacentHTML("beforeend",markup)
-     } catch (error) {
+
+       document.querySelector(".featured__controls").insertAdjacentHTML("beforeend",markup)
+     } 
+     catch (error) 
+     {
          console.log(error)
  
      }
- }
+}
  
- 
- 
- test()
- console.log("test")
+test()
+console.log("test")
